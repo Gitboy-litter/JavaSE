61ad72f1950062fa8ded7615c7dc200ce92e6d98
Gitboy-litter <1541339405@qq.com>
2020-08-27 16:30:45 +0800

    新加的测试类

--- src/day17_test/test2/Test.java
+++ src/day17_test/test2/Test.java
@@ -1,7 +1,12 @@
 package day17_test.test2;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        System.out.println("hello");
+        System.out.println("hello");
         System.out.println("hello");
     }
 }
